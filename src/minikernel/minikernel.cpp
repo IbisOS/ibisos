bff671d701b5d9e3fb59bf1a95cb8e531822a5e9
1dime <onedimemobile@gmail.com>
2021-01-20 04:33:31 -0700

    removed debug lines in minikernel

--- src/minikernel/minikernel.cpp
+++ src/minikernel/minikernel.cpp
@@ -18,17 +18,4 @@ void initiate_mini_kernel(unsigned int *multiboot_struct)
     init_vesa(multiboot_struct);
     //Initialize all PCI devices
     initialize_pci_devices();
-    //Create a simple device
-    struct vfs_device *device = create_device("Rocketeer");
-    //Create a folder
-    device->create_folder("/", (uint16_t *) 0x000000, device);
-    device->create_folder("/test", (uint16_t *) 0x100, device);
-    //Get root folder
-    struct vfs_folder_node* folder = device->find_folder_node("/", device);
-    //Check if folder is not null
-    if(folder != NULL)
-    {
-        //Draw a white circle
-        draw_circle((uint32_t*) multiboot_struct[22], 100, 100, 100, 0xFFFFFF);
-    }
 }
