217eb76837d97ca4e7a0d1b6a27756e162446882
1dime <onedimemobile@gmail.com>
2023-01-05 16:04:39 -0700

    Removed CMakeFiles, cache, cmake_install, and Makefile

--- CMakeFiles/binutils.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/binutils"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/binutils.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- CMakeFiles/gcc.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/gcc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/gcc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
