7a301bfb14c8e2519304d5af9cfba39d52435214
1dime <onedimemobile@gmail.com>
2023-01-05 15:10:13 -0700

    Fixed issue with build script
    Build script was not working, so was fixed.

--- CMakeFiles/IbisOS.bin.dir/string.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/IbisOS.bin.dir/string.cpp.o: \
-  /home/dashiel/Desktop/OffDriveProjects/IbisOS/src/string.cpp \
-  /home/dashiel/Desktop/OffDriveProjects/IbisOS/src/include/cstring \
-  /home/dashiel/Desktop/OffDriveProjects/IbisOS/src/include/c/string.h \
-  /home/dashiel/Desktop/OffDriveProjects/IbisOS/src/include/c/stddef.h
